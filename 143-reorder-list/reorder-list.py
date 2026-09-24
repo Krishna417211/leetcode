67af2456170bd16b64fe477db3e143048eb5fb82
Krishna Priya <blacktheta20@gmail.com>
2026-09-24 10:21:19 +0530

    Time: 3 ms (50.28%) | Memory: 28.1 MB (7.08%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,31 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return None
+
+        slow = fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        temp = slow.next
+        slow.next = None
+
+        prev = None
+        while temp:
+            temp.next, prev, temp = prev, temp, temp.next
+
+        first = head
+        second = prev
+        
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second  = temp2
+
