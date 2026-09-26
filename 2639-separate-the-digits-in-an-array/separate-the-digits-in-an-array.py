1a14a4adf78d208cedebd6e6de3c06c81c226996
Krishna Priya <blacktheta20@gmail.com>
2026-09-26 10:32:50 +0530

    Time: 3 ms (73.76%) | Memory: 19.3 MB (97.77%) - LeetSync

--- 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
+++ 2639-separate-the-digits-in-an-array/separate-the-digits-in-an-array.py
@@ -3,12 +3,10 @@ class Solution:
         val = []
 
         for i in nums:
-            if len(str(i)) > 1:
+            
                 for j in str(i):
                     val.append(int(j))
 
-            else:
-                val.append(i)
-
+            
         return val
         
